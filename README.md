e08c66a2ca070612ca7296a3fc880793ebd6228c
johanIbtaten <48206606+johanIbtaten@users.noreply.github.com>
2020-03-23 19:26:56 +0100

    Initialize project using Webpack

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ma Météo
+
+Weather app using web components and vanilla JavaScript. No framework here. 
+
+# Install & Use
+
+1. Add the following line at the end of your HTML document 
+```html
+<script src="https://rawgithub.com/darkskyapp/skycons/master/skycons.js"></script>
+```
+
+2. Add the following line in the HEAD of your HTML document
+```html
+<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700,800,900&display=swap" rel="stylesheet">
+```
+
+3. Profit!
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+
+    ],
+  },
+  plugins: [
+    new uglifyJsPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+};
