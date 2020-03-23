3f362a1a8872d905945a70e70d23b54758b45efe
johanIbtaten <48206606+johanIbtaten@users.noreply.github.com>
2020-03-23 19:53:28 +0100

    feat(component): implement MeteoCities component

---
+++ src/components/MeteoCities/MeteoCities.component.js
@@ -0,0 +1,58 @@
+import styles from './MeteoCities.styles.scss';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>${styles.toString()}</style>
+  <ul class="cities">    
+    <slot />
+  </ul>
+`;
+
+class MeteoCities extends HTMLElement {
+
+  set datasToDisplay(value) {
+    this._datasToDisplay = value;   
+    this.renderForecast();
+  }
+  
+  get datasToDisplay() {
+    return this._datasToDisplay;
+  }
+
+  set type(value) {
+    this._type = value; 
+  }
+  
+  get type() {
+    return this._type;
+  }
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  renderForecast() {
+    console.log('type', this._type);  ///////////////////////////////////
+    console.log('renderForecast', this._datasToDisplay); ///////////////////////////////////
+
+    this.innerHTML = '';
+
+    this._type = (this._type === 'forecast') ? 'small-card' : 'big-card';
+
+    this._datasToDisplay.forEach( (item, index) => {     
+      const card = document.createElement('meteo-city-card');
+      card.classList.add('fadein');
+      card.classList.add(this._type);
+      card.type = this._type;
+      card.datas = item;
+      this.appendChild(card);
+    })
+  }
+
+}
+
+export default MeteoCities
